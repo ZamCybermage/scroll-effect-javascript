087ae6caa831c6f1e7a78754e47d5e2a00f4ddde
ZamCybermage <zamemdesigns@gmail.com>
2018-11-07 02:51:21 -0500

    gitignore added

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+// smoothe scroll
+
+function scrollEffect(target,duration){
+    //selecting the target
+    var target = document.querySelector(target);
+    //relative scroll position
+    var targetDestination = target.getBoundingClientRect().top;
+   //initial start position
+    var initPosition = window.pageYOffset;
+  //distance between the two
+    var _distance  = targetDestination - initPosition;
+  //starting time
+    var _initTime = null;
+}
+
+
+function scrollAnimation(){
+  
+}
+
+
+//Animation of function duration
+
+scrollEffect('.container-1', 1000);
